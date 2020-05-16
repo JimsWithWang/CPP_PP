34218b58c897eaeb5a4943aad5d0c6678afb136c
JimsWithWang <604473522@qq.com>
2020-05-16 17:51:43 +0800

    提交第十章例题
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 10/examples/10_1_2_3/build/CMakeFiles/10_1_2_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10_1_2_3.dir/src/main.cpp.o"
+  "CMakeFiles/10_1_2_3.dir/src/stock.cpp.o"
+  "10_1_2_3.pdb"
+  "10_1_2_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_1_2_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10/examples/10_4_5_6/src/stock.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "stock.h"
+
+Stock::Stock()
+{
+    std::cout<<"Default constructor called\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+Stock::Stock(const std::string &co, long n, double pr)
+{
+    company = co;
+    if(n<0)
+    {
+        std::cout<<"Number of shares can's be negative; "<<company<<" shares set to 0.\n";
+        shares = 0;
+    }
+    else
+    {
+        shares = n;
+    }
+    share_val = pr;
+    set_tot();
+}
+
+Stock::~Stock()
+{
+    std::cout<<"Bye, "<<company<<std::endl;
+}
+
+void Stock::buy(long num, double price)
+{
+    if(num<0)
+    {
+        std::cout<<"Number of shares purchased can't be nagative. "<<"Transaction is aborted.\n";
+    }
+    else
+    {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price)
+{
+    using std::cout;
+    if(num<0)
+    {
+        std::cout<<"Number of shares sold can't be nagative. "<<"Transaction is aborted.\n";
+    }
+    else if(num>shares)
+    {
+        cout<<"You can't sell more than you have!"<<"Transaction is aborted.\n";
+    }
+    else
+    {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price)
+{
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show()
+{
+    std::cout<<"Company: "<<company<<" shares: "<<shares<<std::endl;
+    std::cout<<" Share Price: $"<<share_val<<" Total Worth: $"<<total_val<<std::endl;
+}
