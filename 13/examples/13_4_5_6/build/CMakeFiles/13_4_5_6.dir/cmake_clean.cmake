f0914dafe463eac88533cc417e25673c21563569
JimsWithWang <604473522@qq.com>
2020-05-20 23:09:14 +0800

    添加第十三章例题4、5和6
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 13/examples/13_4_5_6/build/CMakeFiles/13_4_5_6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13_4_5_6.dir/src/main.cpp.o"
+  "CMakeFiles/13_4_5_6.dir/src/tabtennis.cpp.o"
+  "13_4_5_6.pdb"
+  "13_4_5_6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13_4_5_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
