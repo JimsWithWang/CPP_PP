54307c3ad074359e5a10671eeed2a15a05214ba8
JimsWithWang <604473522@qq.com>
2020-05-20 23:11:02 +0800

    添加第十三章例题14、15和16
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 13/examples/13_14_15_16/build/CMakeFiles/13_14_15_16.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/60447/Documents/CPP_PP/13/examples/13_14_15_16/src/dma.cpp" "/mnt/c/Users/60447/Documents/CPP_PP/13/examples/13_14_15_16/build/CMakeFiles/13_14_15_16.dir/src/dma.cpp.o"
+  "/mnt/c/Users/60447/Documents/CPP_PP/13/examples/13_14_15_16/src/main.cpp" "/mnt/c/Users/60447/Documents/CPP_PP/13/examples/13_14_15_16/build/CMakeFiles/13_14_15_16.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 13/examples/13_14_15_16/build/CMakeFiles/13_14_15_16.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13_14_15_16.dir/src/dma.cpp.o"
+  "CMakeFiles/13_14_15_16.dir/src/main.cpp.o"
+  "13_14_15_16.pdb"
+  "13_14_15_16"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13_14_15_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
