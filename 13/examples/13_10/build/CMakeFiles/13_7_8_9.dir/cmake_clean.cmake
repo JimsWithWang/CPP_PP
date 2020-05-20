eb1c04c822250d38f96efbe55eee91ac74e5f96f
JimsWithWang <604473522@qq.com>
2020-05-20 23:10:10 +0800

    添加第十三章例题10
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 13/examples/13_10/build/CMakeFiles/13_7_8_9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13_7_8_9.dir/src/brass.cpp.o"
+  "CMakeFiles/13_7_8_9.dir/src/main.cpp.o"
+  "13_7_8_9.pdb"
+  "13_7_8_9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13_7_8_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
