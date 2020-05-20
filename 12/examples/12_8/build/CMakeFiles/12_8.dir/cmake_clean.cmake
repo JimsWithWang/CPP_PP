73185b0eb44764cb9424da50d7063e7218bbd301
JimsWithWang <604473522@qq.com>
2020-05-20 11:27:44 +0800

    添加第十二章例题8
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 12/examples/12_8/build/CMakeFiles/12_8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12_8.dir/src/main.cpp.o"
+  "12_8.pdb"
+  "12_8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
