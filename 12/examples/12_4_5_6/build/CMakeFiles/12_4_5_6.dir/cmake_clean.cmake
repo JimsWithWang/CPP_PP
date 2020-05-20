b1ff3fbfaf5b5fffa216270e7c8966613e73b573
JimsWithWang <604473522@qq.com>
2020-05-20 11:26:19 +0800

    添加第十二章例题4、5和6
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 12/examples/12_4_5_6/build/CMakeFiles/12_4_5_6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12_4_5_6.dir/src/main.cpp.o"
+  "CMakeFiles/12_4_5_6.dir/src/string1.cpp.o"
+  "12_4_5_6.pdb"
+  "12_4_5_6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_4_5_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
