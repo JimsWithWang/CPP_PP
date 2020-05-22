f37a7af215efee631482270594a0220085fe1697
JimsWithWang <604473522@qq.com>
2020-05-22 23:57:43 +0800

    添加第十四章例题15和16
    Signed-off-by: JimsWithWang <604473522@qq.com>

---
+++ 14/examples/14_15_16/build/CMakeFiles/14_15_16.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/14_15_16.dir/src/main.cpp.o"
+  "14_15_16.pdb"
+  "14_15_16"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14_15_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
